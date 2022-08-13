4906ccc4f0dc9ddf629bb84c1e97192680f29b31
EC2 Default User <ec2-user@ip-172-31-17-206.ap-northeast-1.compute.internal>
2022-08-13 10:41:36 +0000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: "homes#hello"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/home/msg' => 'homes#msg', as: 'msg'
+  get '/home/plus' => 'homes#plus', as: 'plus'
 end
